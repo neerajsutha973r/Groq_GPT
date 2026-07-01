bde5b76ae0b1f276073b5d022740e917dc7ae421
Neeraj <neerajsuthar227@gmail.com>
2026-07-01 17:36:43 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
-import {MyContext} from "./MyContext.jsx";
 import { useState } from 'react';
 import {v1 as uuidv1} from "uuid";
+import { MyContext } from "./MyContext.jsx";
 
 function App() {
   const [prompt, setPrompt] = useState("");
