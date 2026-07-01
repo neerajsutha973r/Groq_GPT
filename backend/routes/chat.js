5a4ccf7e999536169f40b7a6576aec56a89bfa9f
Neeraj <neerajsuthar227@gmail.com>
2026-07-01 17:06:27 +0530

    first commit

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Thread from "../models/Thread.js";
+import Thread from "../models/thread.js";
 import getGroqAPIResponse from "../utils/groqai.js";
 
 const router = express.Router();
